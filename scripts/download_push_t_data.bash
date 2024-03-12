8dd975fec7e57bf2a3cef4d95cca8f1673a0dc48
Bryson Jones <bryson.jones@jpl.nasa.gov>
2024-03-12 20:35:42 +0000

    Feature/init project

---
+++ scripts/download_push_t_data.bash
@@ -0,0 +1,2 @@
+python ./scripts/download_gdrive_data.py --file_id "1KY1InLurpMvJDRb14L9NlXT_fEsCvVUq&confirm=t" --output_path "./data/push_t_data.zarr.zip"
+unzip ./data/push_t_data.zarr.zip -d ./data/push_t_data.zarr
