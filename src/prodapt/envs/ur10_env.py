f756bd11ac613c784b96f1e2368d2093d78a40b9
Federico Pizarro Bejarano <bejarano@jpl.nasa.gov>
2024-05-02 17:46:03 +0000

    Adding joint-position control with inverse kinematics

--- src/prodapt/envs/ur10_env.py
+++ src/prodapt/envs/ur10_env.py
@@ -1,34 +1,49 @@
 import numpy as np
 import gymnasium as gym
 import rclpy
-import time
 
-from spacenav_converter.spacenav_converter.joint_states_subscriber import (
+from prodapt.envs.ur10_ros_interface.joint_states_subscriber import (
     JointStatesSubscriber,
 )
-from spacenav_converter.spacenav_converter.movel_publisher import MovelPublisher
+from prodapt.envs.ur10_ros_interface.movel_publisher import MovelPublisher
+from prodapt.envs.ur10_ros_interface.joints_publisher import JointsPublisher
 
 
 class UR10Env(gym.Env):
-    def __init__(self):
+    def __init__(self, controller):
         rclpy.init()
         self.joint_state_subscriber = JointStatesSubscriber()
-        self.movel_publisher = MovelPublisher()
+
+        if controller == "movel":
+            self.command_publisher = MovelPublisher()
+        elif controller == "joints":
+            self.command_publisher = JointsPublisher()
+
+        self.last_joint_pos = None
+        self.reset_joint_pos = np.array(
+            [[0.2948, -1.4945, -2.1491, -1.0693, 1.5693, -1.2760]]
+        )
 
     def close(self):
         self.joint_state_subscriber.destroy_node()
-        self.movel_publisher.destroy_node()
+        self.command_publisher.destroy_node()
         rclpy.shutdown()
 
     def reset(self):
-        self.movel_publisher.send_action([0.6, 0, 0.4, 3.14, 0, 0])
-        time.sleep(10)
+        self.command_publisher.send_action(
+            action=[0.6, 0, 0.4, 3.14, 0, 0],
+            duration=10,
+            last_state=self.reset_joint_pos,
+        )
         obs = self._get_latest_observation()
         return obs, {}
 
     def step(self, action):
-        self.movel_publisher.send_action(action)
-        time.sleep(0.1)
+        if self.last_joint_pos is None:
+            self._get_latest_observation()
+        self.command_publisher.send_action(
+            action=action, duration=0.1, last_state=self.last_joint_pos
+        )
         obs = self._get_latest_observation()
         return obs, 0, False, False, {}
 
@@ -42,4 +57,5 @@ class UR10Env(gym.Env):
         [pos, vel, eff] = self.joint_state_subscriber.last_seen_state
 
         obs = np.concatenate((pos, vel, eff))
+        self.last_joint_pos = pos
         return obs
